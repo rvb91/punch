08325399dac219b538f9f7b94c051a760fc289ff
Raghav Bagla <raghav.bagla@gmail.com>
2019-10-07 18:52:09 -0400

    generate scaffold for event model

---
+++ db/migrate/20191007225104_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.references :user, null: false, foreign_key: true
+      t.datetime :clockin_time
+      t.datetime :clockout_time
+
+      t.timestamps
+    end
+  end
+end
