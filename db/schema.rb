9da56231c7c96aeb8556fcd70a72097cd16431d2
Raghav Bagla <raghav.bagla@gmail.com>
2019-10-07 18:59:56 -0400

    schema update for create events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_223839) do
+ActiveRecord::Schema.define(version: 2019_10_07_225104) do
+
+  create_table "events", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "clockin_time"
+    t.datetime "clockout_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,4 +27,5 @@ ActiveRecord::Schema.define(version: 2019_10_07_223839) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "events", "users"
 end
