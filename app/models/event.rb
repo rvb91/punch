bdbace9351f63ab136f05eccece4bfcab6985fdb
Raghav Bagla <raghav.bagla@gmail.com>
2019-10-07 19:50:03 -0400

    add more event validations and ui enhancements

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,20 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :user_id, :clockin_time
+  validate :clockout_gte_clockin
+
+  def self.pending_clockout
+    Event.where(clockout_time:nil).first
+  end
+
+  def clockout_gte_clockin
+    if clockout_time && clockin_time && clockout_time <= clockin_time
+      errors.add(:clockout_time, "can't be less than clock in time")
+    end
+  end
+
+  def prevent_event_overlap
+    raise NotImplementedError
+  end
 end
