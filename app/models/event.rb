5cb6cfce9e3212e34715d19b22a7e35fe759b4cb
Raghav Bagla <raghav.bagla@gmail.com>
2019-10-07 22:26:29 -0400

    clock in out logic on users.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,6 @@ class Event < ApplicationRecord
   validates_presence_of :user_id, :clockin_time
   validate :clockout_gte_clockin
 
-  def self.pending_clockout
-    Event.where(clockout_time:nil).first
-  end
-
   def clockout_gte_clockin
     if clockout_time && clockin_time && clockout_time <= clockin_time
       errors.add(:clockout_time, "can't be less than clock in time")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+
+  has_many :events
+
+  def pending_clockout
+    Event.where(user: self, clockout_time: nil).first
+  end
+
+  def clock_in_out
+    if event = pending_clockout
+      event.clockout_time = Time.now
+      event.save
+    else
+      event = self.events.create!(
+        clockin_time: Time.now,
+      )
+    end
+    event
+   end
+
 end
